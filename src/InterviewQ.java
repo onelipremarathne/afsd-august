46f9b8b80c92c75bb47fd3a75c075696249bbc45
onelipremarathne <ovpremarathne@students.nsbm.ac.lk>
2025-09-28 12:12:03 +0530

    added sout

--- src/InterviewQ.java
+++ src/InterviewQ.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
 
 public class InterviewQ {
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
-        Scanner scan=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("Grade Calculator! :)");
         System.out.print("Enter your grade: ");
-        int mark=scan.nextInt();
+        int mark = scan.nextInt();
 
-        if(mark>=75){
+        if (mark >= 75) {
             System.out.print("Your grade is A");
-        } else if (mark>=65){
+        } else if (mark >= 65) {
             System.out.print("Your grade is B");
-        } else if (mark>=45){
+        } else if (mark >= 45) {
             System.out.print("Your grade is C");
         } else {
             System.out.print("Your grade is F");
         }
+        System.out.println("hii");
     }
 }
